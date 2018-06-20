4eab921a0957b4981d308b757b65796c96649f92
yilong <yilong@simpletech.com.cn>
2018-06-20 09:56:14 +0800

    fix:fixed extra charater :bug:

--- template/build/webpack.base.conf.js
+++ template/build/webpack.base.conf.js
@@ -30,7 +30,7 @@ module.exports = {
   },
   output: {
     path: config.build.assetsRoot,
-    filename: '[name].[hash].js',
+    filename: '[name].js',
     publicPath: process.env.NODE_ENV === 'production'
       ? config.build.assetsPublicPath
       : config.dev.assetsPublicPath
