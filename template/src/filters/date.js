a456017345be32ab678317d2534098d3113009ca
HongleiChan <HongLeiChan@163.com>
2019-07-26 15:44:12 +0800

    babel升级至7.0.0

--- template/src/filters/date.js
+++ template/src/filters/date.js
@@ -1,43 +1,42 @@
 // 这里放置一些Vue自定义Filter
 export const formatDate = time => {
-  if(time){
+  if (time) {
     let tmpDate = new Date(time)
     let year = tmpDate.getFullYear()
     let mathon = tmpDate.getMonth() + 1
-    if (mathon < 10){
+    if (mathon < 10) {
       mathon = '0' + mathon
     }
     let day = tmpDate.getDate()
-    if ( day < 10 ){
+    if (day < 10) {
       day = '0' + day
     }
     return year + '-' + mathon + '-' + day
-  }
-  else {
+  } else {
     return ''
   }
 }
 
-export const formatTime =  time => {
-  if(time === null || time === ''){
+export const formatTime = time => {
+  if (time === null || time === '') {
     return ''
   }
   let tmpDate = new Date(time)
   let year = tmpDate.getFullYear()
   let mathon = tmpDate.getMonth() + 1
-  if (mathon < 10){
+  if (mathon < 10) {
     mathon = '0' + mathon
   }
   let day = tmpDate.getDate()
-  if ( day < 10 ){
+  if (day < 10) {
     day = '0' + day
   }
   let hours = tmpDate.getHours()
-  if ( hours < 10 ){
+  if (hours < 10) {
     hours = '0' + hours
   }
   let minutes = tmpDate.getMinutes()
-  if ( minutes < 10 ){
+  if (minutes < 10) {
     minutes = '0' + minutes
   }
   return year + '-' + mathon + '-' + day + ' ' + hours + ':' + minutes
